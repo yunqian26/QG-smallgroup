68236143784c284e14dc92a9b447e41e14f26ee7
yunqian26 <yunqian1271556864@163.com>
2025-03-16 20:27:24 +0800

    1

--- Week_1/code/最小二乘法.py
+++ Week_1/code/最小二乘法.py
@@ -5,7 +5,7 @@ def minplus2(x, y):
     coefficients, residuals, rank, s = np.linalg.lstsq(A, y, rcond=-1)
     x_coeff, y_coeff = coefficients
     print(f"拟合的直线方程为：y = {x_coeff:.2f}x + {y_coeff:.2f}")
-    plt.scatter(x, y, color='b')
+    # plt.scatter(x, y, color='b')
     x_fit = np.linspace(min(x), max(x), num=20)
     y_fit = x_coeff * x_fit + y_coeff
     plt.plot(x_fit, y_fit, color='red', label=f'拟合曲线: y = {x_coeff:.2f}x+ {y_coeff:.2f}')
@@ -14,4 +14,5 @@ def minplus2(x, y):
 if __name__ == '__main__':
     a=np.array([1, 2, 6, 7, 5,2])
     b = np.array([2, 1, 5, 4, 5,8])
+    plt.scatter(a, b, color='b')
     minplus2(a,b)
